cc7f936d1bd51640cf633f0d42037ba88d6f2866
luisf11 <luisfernandop1110@gmail.com>
2017-07-05 14:26:23 -0400

    about me done

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,19 @@
 window.onload = function() { document.body.className = ''; }
 window.ontouchmove = function() { return false; }
 window.onorientationchange = function() { document.body.scrollTop = 0; }
+
+
+// typed.js configuration
+$(function(){
+  $(".type").typed({
+    strings: [
+      "an adventurer",
+      "a software developer",
+      "a creator",
+      "an enthusiast"
+    ],
+    typeSpeed: 70,
+    backDelay: 200,
+    loop: true
+  })
+})

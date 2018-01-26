7590fc9fd1b3e333d93cbe02fe62afba9929d519
luisf11 <luisfernandop1110@gmail.com>
2018-01-26 10:11:43 -0400

    Add loading spinner

--- porfolio/src/App.js
+++ porfolio/src/App.js
@@ -5,6 +5,17 @@ import Home from './modules/home'
 
 
 class App extends Component {
+  componentDidMount(){
+    const ele = document.getElementById('ipl-progress-indicator')
+    if(ele){
+      setTimeout(() => {
+        ele.classList.add('available')
+        setTimeout(() => {
+          ele.outerHTML = ''
+        }, 2000)
+      }, 1000)
+    }
+  }
   render() {
     return (
       <div>
